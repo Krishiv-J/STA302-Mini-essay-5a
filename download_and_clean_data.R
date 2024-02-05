e789a14e6e8de30bfc5121afef8a11284bc13d0a
Krishiv-J <krishivjain1234@gmail.com>
2024-02-05 01:22:51 -0500

    Debugging

--- download_and_clean_data.R
+++ download_and_clean_data.R
@@ -7,7 +7,7 @@ library(knitr)
 
 raw_data <-
   read_html(
-    "https://en.wikipedia.org/wiki/List_of_prime_ministers_of_New_Zealand"
+    "https://en.wikipedia.org/wiki/List_of_prime_ministers_of_India"
   )
 write_html(raw_data, "pms.html")
 
@@ -19,17 +19,15 @@ parse_data_selector_gadget <-
 parsed_data <-
   parse_data_selector_gadget |> 
   clean_names() |> 
-  rename(raw_text = name_constituency_birth_death) |> 
+  rename(raw_text = name_born_died_constituency) |> 
   select(raw_text) |> 
-  filter(raw_text != "Name(Birth–Death)Constituency") |> 
+  filter(raw_text != "Name(born – died)Constituency") |> 
   distinct() 
-
-parsed_data <- parsed_data[-1, ] # Deleting first row
 
 initial_clean <-
   parsed_data |>
   separate(
-    raw_text, into = c("name", "not_name"), sep = "\\,", extra = "merge",
+    raw_text, into = c("name", "not_name"), sep = "\\(", extra = "merge",
   ) |> 
   mutate(date = str_extract(not_name, "[[:digit:]]{4}–[[:digit:]]{4}"),
          born = str_extract(not_name, "born[[:space:]][[:digit:]]{4}")
@@ -57,3 +55,21 @@ cleaned_data |>
   )
 
 write_csv(cleaned_data, "cleaned_data.csv")
+
+cleaned_data |>
+  mutate(
+    still_alive = if_else(is.na(died), "Yes", "No"),
+    died = if_else(is.na(died), as.integer(2023), died)
+  ) |>
+  mutate(name = as_factor(name)) |>
+  ggplot(
+    aes(x = born, xend = died, y = name, yend = name, color = still_alive)
+  ) +
+  geom_segment() +
+  labs(
+    x = "Year of birth", y = "Prime minister", color = "PM is currently alive"
+  ) +
+  theme_minimal() +
+  scale_color_brewer(palette = "Set1") +
+  theme(legend.position = "bottom")
+
